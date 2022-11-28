d646de2fa12ed630de4ace8164dda353d2c8292d
amina niyazbek <107414625+aminokn@users.noreply.github.com>
2022-11-28 16:55:54 +0600

    Add files via upload

---
+++ assignment 4/math/2.py
@@ -0,0 +1,6 @@
+height, base1, base2 = 5, 5, 6
+area = (base1 + base2) * height / 2
+print("Height: ", height)
+print("Base, first value: ", base1)
+print("Base, second value: ", base2)
+print("Expected Output: ", area)
---
+++ assignment 4/math/3.py
@@ -0,0 +1,5 @@
+base, height = 5, 6
+area = base * height
+print("Length of base: ", base)
+print("Height of parallelogram: ", height)
+print("Expected Output: ", "%.1f" % area)
