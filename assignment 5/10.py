8c78053cbfde725842ebd4abdbb7270a6ba92658
amina niyazbek <107414625+aminokn@users.noreply.github.com>
2022-12-05 01:45:47 +0600

    Add files via upload

---
+++ assignment 5/10.py
@@ -0,0 +1,6 @@
+import re
+text = input()
+def convert(text):
+    pattern = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', text)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', pattern).lower()
+print(convert(text))
